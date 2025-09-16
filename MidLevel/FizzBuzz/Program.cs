bcb391c2c6e7beee70dc98b0baff6a51056f8d6a
Adam Jacobs <adam.alfray.jacobs@gmail.com>
2025-09-16 19:56:07 +0100

    Create the FizzBuzz rule set

--- MidLevel/FizzBuzz/Program.cs
+++ MidLevel/FizzBuzz/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a range of numbers from 1 to 100
+
+            // For each number in the range, do the following:
+            //    If the number is divisible by 3, print "Fizz"
+            //    If the number is divisible by 5, print "Buzz"
+            //    If the number is divisible by both 3 and 5, print "FizzBuzz"
+        }
+    }
+}
