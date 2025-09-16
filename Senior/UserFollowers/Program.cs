35da4e0854bb5ff8ed9eb480e5e14bfcabba0a35
Adam Jacobs <adam.alfray.jacobs@gmail.com>
2025-09-16 20:14:19 +0100

    Cleanup and add user followers question

---
+++ Senior/UserFollowers/Program.cs
@@ -0,0 +1,11 @@
+﻿//Thinking problem:
+
+//In your database, you have a list of users. 
+//Each user can follow other users.
+//How would you design a database schema to represent this relationship?
+
+//How would you scale such a database?
+
+
+//Write a function (query or code) to find the top 10 users whose followers have the most followers. 
+//Eg. If Adam has 10 followers and each of Adam's followers has 100 followers, then Adam has a score of 1,000.
