d125343411d12d4992180fb7c16d60f25434fb3c
Adam Jacobs <adam.alfray.jacobs@gmail.com>
2025-09-16 20:06:47 +0100

    Add array accessor question

---
+++ MidLevel/ArrayAccessor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayAccessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console output the fruit with the highest cost
+
+            //Console output the fruit with the longest name
+
+            //Console output all yellow fruits
+        }
+
+        record Fruit(string Name, string Color, int Cost);
+
+        static List<Fruit> Fruits = new List<Fruit>
+        {
+            new Fruit("Apple", "Red", 3),
+            new Fruit("Banana", "Yellow", 1),
+            new Fruit("Grapes", "Purple", 2),
+            new Fruit("Orange", "Orange", 2),
+            new Fruit("Mango", "Yellow", 5)
+        };
+    }
+}
