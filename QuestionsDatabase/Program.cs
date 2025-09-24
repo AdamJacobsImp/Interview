68f730ccfdd29f100891b4a01da5199ac6679f8d
Adam Jacobs <adam.jacobs@improveasy.com>
2025-09-24 07:36:11 +0100

    Add question about question set storage

---
+++ QuestionsDatabase/Program.cs
@@ -0,0 +1,10 @@
+﻿// Thinking question:
+
+// You have the requirement to display questions on screen. The questions that have been requested are below:
+
+// When was your home built?
+// What type of property do you live in (terraced, semi-detached, detached, bungalow)
+// How many windows do you have
+// What material are you windows (PVC, aluminium, wood, other)
+
+// How would you display these and store the results ? 
